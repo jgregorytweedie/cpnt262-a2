ad5bd3c29a802ece9f3bbcb94b9a1ed86d09b287
jgregorytweedie <71092974+jgregorytweedie@users.noreply.github.com>
2020-11-01 16:31:05 -0700

    added in arrays in client, added in commas in animals

--- js/animals.js
+++ js/animals.js
@@ -1,9 +1,12 @@
 
 export const animals = [
 //so what i want to do is just add in width and height properties, as well as the path of each photo.
+// not going to lie, this was a pain in the ass to go to every single one and add in the width and height attributes but oh well....
   {
     id:     237,
     title:  'Puppy',
+    credit: 'André Spieker',
+    url:    'https://unsplash.com/photos/8wTPqxlnKM4',
     width: '350',
     height: '210',
     path: 'assets/images/puppy.jpg'
@@ -11,6 +14,8 @@ export const animals = [
   {
     id:     433,
     title:  'Bear',
+    credit: 'Thomas Lefebvre',
+    url:    'https://unsplash.com/photos/aRXPJnXQ9lU',
     width: '350',
     height: '233',
     path: 'assets/images/bear.jpg'
@@ -18,6 +23,8 @@ export const animals = [
   {
     id:     577,
     title:  'Moose',
+    credit: 'Thomas Lefebvre',
+    url:    'https://unsplash.com/photos/34mvN5nt1ls',
     width: '350',
     height: '233',
     path: 'assets/images/moose.jpg'
@@ -25,6 +32,8 @@ export const animals = [
   {
     id:     582,
     title:  'Coyote',
+    credit: 'Levi Saunders',
+    url:    'https://unsplash.com/photos/NUMlxTPsznM',
     width: '350',
     height: '233',
     path: 'assets/images/coyote.jpg'
@@ -32,6 +41,8 @@ export const animals = [
   {
     id:     593,
     title:  'Tiger',
+    credit: 'Paula Borowska',
+    url:    'https://unsplash.com/photos/OM7CvKnhjfs',
     width: '263',
     height: '350',
     path: 'assets/images/tiger.jpg'
@@ -39,6 +50,8 @@ export const animals = [
   {
     id:     659,
     title:  'Husky',
+    credit: 'Alexander Dimitrov',
+    url:    'https://unsplash.com/photos/KGy1GFeKBu4',
     width: '350',
     height: '197',
     path: 'assets/images/husky.jpg'
@@ -47,6 +60,8 @@ export const animals = [
   {
     id:     718,
     title:  'Wolf',
+    credit: 'Josh Felise',
+    url:    'https://unsplash.com/photos/mblYxasm0nk',
     width: '350',
     height: '222',
     path: 'assets/images/wolf.jpg'
@@ -54,6 +69,8 @@ export const animals = [
   {
     id:     783,
     title:  'Monkey',
+    credit: 'Damir Kotorić',
+    url:    'https://unsplash.com/photos/cSQw4uqF_Hc',
     width: '350',
     height: '233',
     path: 'assets/images/monkey.jpg'
@@ -61,6 +78,8 @@ export const animals = [
   {
     id:     790,
     title:  'Elk',
+    credit: 'rebecca johnston',
+    url:    'https://unsplash.com/photos/nuRYwOJryyk',
     width: '350',
     height: '233',
     path: 'assets/images/elk.jpg'
@@ -68,6 +87,8 @@ export const animals = [
   {
     id:     837,
     title:  'Bulldog',
+    credit: 'Yamon Figurs',
+    url:    'https://unsplash.com/photos/2zlxFmByClI',
     width: '350',
     height: '233',
     path: 'assets/images/bulldog.jpg'
@@ -75,6 +96,8 @@ export const animals = [
   {
     id:     1024,
     title:  'Bird',
+    credit: 'Мартин Тасев',
+    url:    'https://unsplash.com/photos/7ALI0RYyq6s',
     width: '350',
     height: '233',
     path: 'assets/images/bird.jpg'
@@ -82,6 +105,8 @@ export const animals = [
   {
     id:     1025,
     title:  'Pug',
+    credit: 'Matthew Henry',
+    url:    'https://unsplash.com/photos/U5rMrSI7Pn4',
     width: '350',
     height: '233',
     path: 'assets/images/pug.jpg'
@@ -89,6 +114,8 @@ export const animals = [
   {
     id:     1074,
     title:  'Big Cat',
+    credit: 'Samuel Scrimshaw',
+    url:    'https://unsplash.com/photos/sseiVD2XsOk',
     width: '350',
     height: '233',
     path: 'assets/images/cat.jpg'
@@ -96,6 +123,8 @@ export const animals = [
   {
     id:     1084,
     title:  'Walrus',
+    credit: 'Jay Ruzesky',
+    url:    'https://unsplash.com/photos/h13Y8vyIXNU',
     width: '350',
     height: '250',
     path: 'assets/images/walrus.jpg'
--- js/client.js
+++ js/client.js
@@ -1,2 +1,19 @@
 import { animals } from "./animals.js";
 // the above is to import the animals.js file. 
+let outputHTML = '';
+// So what i want to do is create a variable where I can store my html.
+
+const gallery = document.querySelector('.gallery'); //I'm just going to write this down here cus I often forget,
+// queryselector is a way to select a class in CSS. In this case, I am selecting the .gallery class in CSS and making it a variable in JS too.
+//Now what i want to do, is create an array with the animals within my animals.js file.
+animals.forEach(function(animal) {
+  output += `<figure class="figCard"> 
+  <img src="assets/images/${animal.imagePath}.jpg" alt="Lorem picsum picture: ${animal.title}" width="${animal.width}" height="${animal.height}">
+  <figcaption>
+    ${animal.title} by <a href="${animal.url}" target= "_blank">${animal.credit}</a>
+  </figcaption>
+  </figure>`           
+            
+});
+
+gallery.innerHTML = `${output}`;
